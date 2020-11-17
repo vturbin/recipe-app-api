cf531cb6e2765a565cfa58f38c3535880683e780
Valentin Turbins <v.turbins@smart-iot.solutions>
2020-11-17 21:41:29 +0100

    added list recipe feature

--- app/recipe/urls.py
+++ app/recipe/urls.py
@@ -6,6 +6,8 @@ from recipe import views
 router = DefaultRouter()
 router.register('tags', views.TagViewSet)
 router.register('ingredients', views.IngredientViewSet)
+router.register('recipes', views.RecipeViewSet)
+
 app_name = 'recipe'
 
 urlpatterns = [
